6cb4600112b58f3a9622b72dcdf79e9d9d70d2c6
facejiong <2279763407@qq.com>
2018-02-21 19:31:20 +0800

    feat:set three example

--- docs/index.js
+++ docs/index.js
@@ -1,43 +1,4 @@
 import { Chart } from '../src/Chart'
-
-const pieData = [
-	{
-		label: '实例1',
-		value: 234
-	},
-	{
-		label: '实例2',
-		value: 100
-	},
-	{
-		label: '实例3',
-		value: 500
-	},
-		{
-		label: '实例4',
-		value: 400
-	}
-]
-
-let barData = {
-	labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
-		"12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
-
-	datasets: [
-		{
-			title: "Some Data",
-			values: [25, 40, 30, 35, 8, 52, 17, -4]
-		},
-		{
-			title: "Another Set",
-			values: [25, 50, -10, 15, 18, 32, 27, 14]
-		},
-		{
-			title: "Yet Another",
-			values: [15, 20, -3, -15, 58, 12, -17, 37]
-		}
-	]
-};
 const scatterData = [
 	{
 		label: 'a',
@@ -75,22 +36,72 @@ const scatterData = [
 		color: 'rgba(167, 232, 180, 0.5)'
 	}
 ]
-const chart = new Chart({
-	id: "my-chart",
-	title: "My Chart",
+
+// pie
+const pieData = [
+	{
+		label: '实例1',
+		value: 234
+	},
+	{
+		label: '实例2',
+		value: 100
+	},
+	{
+		label: '实例3',
+		value: 500
+	},
+		{
+		label: '实例4',
+		value: 400
+	}
+]
+const pieChart = new Chart({
+	id: 'pie-chart',
+	title: 'Pie Chart',
 	// subtitle: 'My Sub Chart',
 	data: pieData,
 	type: 'pie', // or 'line', 'scatter', 'pie', 'percentage'
 	height: 250,
-	width: 320,
+	width: 640,
 	sortDataType: 'descending', // ascending descending none
-	format_tooltip_x: d => (d + '').toUpperCase(),
-	format_tooltip_y: d => d + ' pts',
-	axis: {
-		color: 'red',
-		xAxisShow: true,
-		yAxisShow: true,
-	}
 })
-console.log(chart)
+console.log(pieChart)
+// bar
+let barData = {
+	labels: ['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04',
+		'2018-01-05', '2018-01-06', '2018-01-07', '2018-01-08'],
 
+	datasets: [
+		{
+			title: '实例1',
+			values: [25, 40, 30, 35, 8, 52, 17, -4]
+		},
+		{
+			title: '实例2',
+			values: [25, 50, -10, 15, 18, 32, 27, 14]
+		},
+		{
+			title: '实例3',
+			values: [15, 20, -3, -15, 58, 12, -17, 37]
+		}
+	]
+};
+const barChart = new Chart({
+	id: 'bar-chart',
+	title: 'Bar Chart',
+	data: barData,
+	type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
+	height: 250,
+	width: 640,
+})
+console.log(barChart)
+const percentageChart = new Chart({
+	id: 'percentage-chart',
+	title: 'Percentage Chart',
+	data: pieData,
+	type: 'percentage', // or 'line', 'scatter', 'pie', 'percentage'
+	height: 250,
+	width: 640,
+})
+console.log(percentageChart)
