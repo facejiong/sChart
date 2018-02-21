48410aba249e1974760862243c83a461a3b0c17c
facejiong <2279763407@qq.com>
2018-02-21 14:46:00 +0800

    feat:change pie svg structure

--- src/charts/PieChart.ts
+++ src/charts/PieChart.ts
@@ -1,8 +1,7 @@
 import Base from './Base'
-import {createText, createPath} from '../plugins/ElementFactory'
+import {createText, createPath, createElement} from '../plugins/ElementFactory'
 import {Option} from '../Option'
 import { COlOR_CLASSFICATION, COLOR_CONTINUOUS_SEMANTIC_DEEPPINK, hoverColor } from '../plugins/ColorSwatches';
-import {Tips} from '../plugins/Tips'
 
 const ANGLE_RATIO = Math.PI / 180;
 const FULL_ANGLE = 360;
@@ -13,32 +12,27 @@ export class PieChart extends Base {
 	private opacity: number;
 	private radius: number;
 	private sortDataType: string;
-	private slices: Array<any>;
-	private labels: Array<string>;
-	private tips: any;
-	private currentSlice: any;
 
 	constructor(option: Option) {
 		super(option)
 		this.type = option.type;
-		this.centerX = this.width / 2;
-		this.centerY = this.height / 2;
 		this.opacity = option.opacity || 0.8;
 		this.colors = option.colors || COlOR_CLASSFICATION;
-		this.radius = option.radius || (this.height > this.width ? this.width * 0.3 : this.height * 0.3);
 		this.sortDataType = option.sortDataType || 'descending';
-		this.slices = [];
-		this.labels = [];
-
+		this.radius = option.radius || 0;
 		this.render()
 	}
+	private setPosition() {
+		this.centerX = this.groupChartWidth / 2;
+		this.centerY = this.groupChartHeight / 2;
+		this.radius = 0 || (this.groupChartHeight > this.groupChartWidth ? this.groupChartWidth * 0.45 : this.groupChartHeight * 0.45);
+	}
 	public render() {
-		console.log('render')
-		this.setWidthHeight();
-		// 顺序 逆序
+		this.renderBase();
+		this.setPosition();
 		this.sortData();
-		this.drawPie();
-		this.addTooltips()
+		this.renderPie();
+		this.renderTips();
 	}
 	private sortData () {
 		if (this.sortDataType === 'descending') {
@@ -51,10 +45,11 @@ export class PieChart extends Base {
 			})
 		}
 	}
-	private drawPie() {
+	private renderPie() {
 		const{centerX,centerY,radius} = this;
 		let totalValue = this.data.reduce((accumulator, current) => accumulator + current.value, 0);
 		let curAngle = 180;
+		let pieElement = createElement('g', {className: 'schart-pie'});
 		this.data.map((current, i) => {
 			const startAngle = curAngle;
 			const diffAngle = (current.value / totalValue) * FULL_ANGLE;
@@ -62,12 +57,13 @@ export class PieChart extends Base {
 			const startPosition = PieChart.getPositionByAngle(startAngle,radius);
 			const endPosition = PieChart.getPositionByAngle(endAngle,radius);
 			const curPath = this.makeArcPath(startPosition, endPosition, diffAngle);
-			let slice = createPath(curPath, 'pie-path', 'none', this.colors[i]);
+			let slice = createPath(curPath, 'schart-pie-path', 'none', this.colors[i]);
 			this.slices.push(slice);
-			this.labels.push(`${current.label}: ${parseInt(String(diffAngle))}%`);
-			slice.style.transition = 'transform .3s;';
-			this.id.appendChild(slice);
+			let percent = diffAngle / 3.6;
+			this.labels.push(`${current.label}: ${parseInt(String(percent))}%`);
+			pieElement.appendChild(slice);
 		})
+		this.svgElement.appendChild(pieElement);
 	}
 	static getPositionByAngle(angle, radius){
 		return {
@@ -75,30 +71,20 @@ export class PieChart extends Base {
 			y:Math.cos(angle * ANGLE_RATIO) * radius,
 		};
 	}
-	makeArcPath(startPosition, endPosition, angle){
+	private makeArcPath(startPosition, endPosition, angle){
 		const{centerX,centerY,radius} = this;
 		return `M${centerX} ${centerY} L${centerX+startPosition.x} ${centerY+startPosition.y} A ${radius} ${radius} 0 ${angle > 180 ? 1 : 0} 0 ${centerX+endPosition.x} ${centerY+endPosition.y} z`;
 	}
-
-	private addTooltips() {
-		this.tips = new Tips();
-		this.slices.map((current) => {
-			current.addEventListener('mousemove',this.mouseMove);
-			current.addEventListener('mouseleave',this.mouseLeave);
-		})
-	}
-	private	mouseMove = (e) => {
+	protected	mouseMove = (e) => {
 		this.slices.map((current, index) => {
 			if (e.target === current) {
 				let hasUpdateData = this.currentSlice === current ? false : true;
 				// console.log(current.style.fill);
 				// current.style.fill = hoverColor(current.style.fill, 15);
-				this.tips.update([{color: current.style.fill, text: this.labels[index]}], e.x, e.y, hasUpdateData);
+				this.tips.update([{color: current.style.fill, text: this.labels[index]}],
+					e.x, e.y, hasUpdateData);
 				this.currentSlice = current;
 			}
 		})
-	}
-	private mouseLeave = () => {
-		this.tips.hide();
 	}
 }
