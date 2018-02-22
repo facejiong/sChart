7f145400a2d92070b5e9ed17d68f334f9a26ce34
facejiong <2279763407@qq.com>
2018-02-22 15:23:26 +0800

    feat:add doc pre code

--- docs/index.js
+++ docs/index.js
@@ -1,21 +1,7 @@
 import { Chart } from '../src/Chart'
-const scatterData = [
-	{label: '实例1',x: 23, y: 34, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例2',x: 213, y: 324, r: 12, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例3',x: 23, y: 314, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例4',x: 23, y: 384, r: 5, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例5',x: 123, y: 734, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例6',x: 33, y: 234, r: 4, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例7',x: 243, y: 134, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例8',x: 53, y: 444, r: 7, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例9',x: 73, y: 504, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例10',x: 23, y: 34, r: 6, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例11',x: 13, y: 84, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
-	{label: '实例12',x: 23, y: 341, r: 7, color: 'rgba(167, 232, 180, 0.5)'},
-]
 
 // pie
-const pieData = [
+const data0 = [
 	{
 		label: '实例1',
 		value: 234
@@ -37,15 +23,25 @@ const pieChart = new Chart({
 	id: 'pie-chart',
 	title: 'Pie Chart',
 	// subtitle: 'My Sub Chart',
-	data: pieData,
+	data: data0,
 	type: 'pie', // or 'line', 'scatter', 'pie', 'percentage'
 	height: 250,
 	width: 640,
 	sortDataType: 'descending', // ascending descending none
 })
 console.log(pieChart)
+
+const percentageChart = new Chart({
+	id: 'percentage-chart',
+	title: 'Percentage Chart',
+	data: data0,
+	type: 'percentage', // or 'line', 'scatter', 'pie', 'percentage'
+	height: 250,
+	width: 640,
+})
+console.log(percentageChart)
 // bar
-let barData = {
+let data1 = {
 	labels: ['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04',
 		'2018-01-05', '2018-01-06', '2018-01-07', '2018-01-08'],
 
@@ -58,16 +54,12 @@ let barData = {
 			title: '实例2',
 			values: [-20, -50, -10, -15, -18, -32, -27, -14]
 		},
-		// {
-		// 	title: '实例3',
-		// 	values: [15, 20, -3, -15, 58, 12, -17, 37]
-		// }
 	]
 };
 const barChart = new Chart({
 	id: 'bar-chart',
 	title: 'Bar Chart',
-	data: barData,
+	data: data1,
 	type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
 	height: 250,
 	width: 640,
@@ -76,12 +68,26 @@ console.log(barChart)
 const lineChart = new Chart({
 	id: 'line-chart',
 	title: 'Line Chart',
-	data: barData,
+	data: data1,
 	type: 'line', // or 'line', 'scatter', 'pie', 'percentage'
 	height: 250,
 	width: 640,
 })
 console.log(lineChart)
+const scatterData = [
+	{label: '实例1',x: 23, y: 34, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例2',x: 213, y: 324, r: 12, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例3',x: 23, y: 314, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例4',x: 23, y: 384, r: 5, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例5',x: 123, y: 734, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例6',x: 33, y: 234, r: 4, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例7',x: 243, y: 134, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例8',x: 53, y: 444, r: 7, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例9',x: 73, y: 504, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例10',x: 23, y: 34, r: 6, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例11',x: 13, y: 84, r: 8, color: 'rgba(167, 232, 180, 0.5)'},
+	{label: '实例12',x: 23, y: 341, r: 7, color: 'rgba(167, 232, 180, 0.5)'},
+]
 const scatterChart = new Chart({
 	id: 'scatter-chart',
 	title: 'Scatter Chart',
@@ -91,12 +97,3 @@ const scatterChart = new Chart({
 	width: 640,
 })
 console.log(scatterChart)
-const percentageChart = new Chart({
-	id: 'percentage-chart',
-	title: 'Percentage Chart',
-	data: pieData,
-	type: 'percentage', // or 'line', 'scatter', 'pie', 'percentage'
-	height: 250,
-	width: 640,
-})
-console.log(percentageChart)
