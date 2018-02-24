3baa08c4683da7528e30ffcc7294ae81ab1b6513
facejiong <2279763407@qq.com>
2018-02-24 15:38:39 +0800

    feat:test ava

--- test/plugins/Coord.test.ts
+++ test/plugins/Coord.test.ts
@@ -3,7 +3,5 @@ import { calculateTicks } from "../../src/plugins/Coord";
 
 test("calculateTicks", (t) => {
   // 断言
-  t.is(calculateTicks(0, 10, 100), {
-
-  });
+  t.is(calculateTicks(0, 10, 100).ceilMax, 10);
 });
